96f90b0de61ff1b6cc829020b92bb4c0af1553b4
Pablo <pgomezvillouta@gmail.com>
2023-12-01 22:30:20 -0300

    uptade

--- movieapp/src/components/Login.jsx
+++ movieapp/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 const formularioLoginJSON = {
   campos: [
@@ -26,58 +26,61 @@ const formularioLoginJSON = {
 };
 
 function Login() {
-    const navigate = useNavigate();
-    const [correo, setCorreo] = useState("");
-    const [clave, setClave] = useState("");
-    const [loginExitoso, setLoginExitoso] = useState(false);
-  
-    const handleCorreoChange = (e) => setCorreo(e.target.value);
-    const handleClaveChange = (e) => setClave(e.target.value);
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      setLoginExitoso(true);
-      setTimeout(() => {
-        setCorreo("");
-        setClave("");
-        setLoginExitoso(false);
-        navigate('/');
-      }, 2000);
-    };
-  
-    return (
-      <div className="contact-container bg-gray-200 border-2 border-black p-3 mt-10 max-w-md mx-auto rounded shadow mb-16">
+  const navigate = useNavigate();
+  const [correo, setCorreo] = useState('');
+  const [clave, setClave] = useState('');
+  const [loginExitoso, setLoginExitoso] = useState(false);
+
+  const handleCorreoChange = (e) => setCorreo(e.target.value);
+  const handleClaveChange = (e) => setClave(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoginExitoso(true);
+    setTimeout(() => {
+      setCorreo('');
+      setClave('');
+      setLoginExitoso(false);
+      navigate('/');
+    }, 2000);
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen"
+    style={{marginTop: "0px"}}>
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h1 className="text-4xl font-bold mt-6 mb-2 items-center justify-center flex">Login</h1>
+        {loginExitoso && <p className="text-green-500 mb-4">Has ingresado correctamente.</p>}
         <div className="flex flex-col items-center">
-          <h1 className="text-4xl font-bold mt-6 mb-2">Login</h1>
-          {loginExitoso && (
-            <p className="text-green-500 mb-4">Has ingresado correctamente.</p>
-          )}
-          <div className="flex flex-col items-center">
-            <form onSubmit={handleSubmit} className="flex flex-col items-stretch mb-16">
-              {formularioLoginJSON.campos.map((campo, index) => (
-                <div key={index} className="mb-3">
-                  <label className="text-lg font-semibold">{campo.etiqueta}</label>
-                  <input
-                    type={campo.tipo}
-                    name={campo.nombre}
-                    className={campo.clase}
-                    onChange={campo.nombre === 'correo' ? handleCorreoChange : handleClaveChange}
-                    value={campo.nombre === 'correo' ? correo : clave}
-                    placeholder={campo.placeholder}
-                  />
-                </div>
-              ))}
-              <button
-                type={formularioLoginJSON.boton.tipo}
-                className={formularioLoginJSON.boton.clase}
-              >
-                {formularioLoginJSON.boton.etiqueta}
-              </button>
-            </form>
-          </div>
+          <form onSubmit={handleSubmit} className="flex flex-col items-stretch mb-16">
+            {formularioLoginJSON.campos.map((campo, index) => (
+              <div key={index} className="mb-3">
+                <label className="text-lg font-semibold">{campo.etiqueta}</label>
+                <input
+                  type={campo.tipo}
+                  name={campo.nombre}
+                  className={campo.clase}
+                  onChange={campo.nombre === 'correo' ? handleCorreoChange : handleClaveChange}
+                  value={campo.nombre === 'correo' ? correo : clave}
+                  placeholder={campo.placeholder}
+                />
+              </div>
+            ))}
+            <button type={formularioLoginJSON.boton.tipo} className={formularioLoginJSON.boton.clase}>
+              {formularioLoginJSON.boton.etiqueta}
+            </button>
+          </form>
+        </div>
+        <div className="items-center justify-center flex"
+        style={{}}>
+          Don't have an account?
+          <Link to="/registrarse" className="font-bold text-blue-500 hover:text-blue-800" style={{ marginLeft: '5px' }}>
+            Sign Up
+          </Link>
         </div>
       </div>
-    );
-  }
-  
-  export default Login;
+    </div>
+  );
+}
+
+export default Login;
